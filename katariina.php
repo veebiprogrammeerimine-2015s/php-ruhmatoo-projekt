55a170f0c7ad3df7ef65c76d3ce48513057523a0
Katariina Linde <klinde@greeny.cs.tlu.ee>
2015-11-16 13:42:02 +0200

    git works

--- katariina.php
+++ katariina.php
@@ -1,5 +1,16 @@
 <?php
 
     echo ("Tere");
+    //git branch
+    //git branch katariinal
+    //git checkout katariinal
+    
+    //git add .
+    //git commit -m ""
+    //git push origin katariinal
 
+    //git checkout master
+    //git merge katariinal - masteril on nüüd katariinal fail
+    //git push
+    //git checkout katariinal
 ?>
