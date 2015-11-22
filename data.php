d88d56cc7dd5d6da4d734a142f85fb2490539b33
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-11-22 03:09:35 +0200

    tried making registering function

--- data.php
+++ data.php
@@ -1,3 +1,34 @@
 <?php
-	//data.php
+	require_once("functions.php");
+	// data.php
+	
+	// kui kasutaja ei ole sisseloginud,
+	// siis suunan tagasi
+	if(!isset($_SESSION["logged_in_user_id"])){
+		header("Location: login.php");
+		
+	}
+	
+	$problem = $date = $animal_kind = $owner_name = $animal_name = "";
+	$problem_error = $date_error = $animal_kind_error = $owner_name_error = $animal_name_error = "";
+	
 ?>
+<p>
+	Tere, <?=$_SESSION["logged_in_user_email"];?> 
+	<a href="?logout=1"> Logi välja <a> 
+</p>
+
+<h2>Registreerimine</h2>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<label for="omaniku_nimi" >Omaniku nimi</label><br>
+	<input id="omaniku_nimi" name="omaniku_nimi" type="text" value="<?php echo $owner_name; ?>"> <?php echo $owner_name_error; ?><br><br>
+	<label for="looma_nimi" >Looma nimi</label><br>
+	<input id="looma_nimi" name="looma_nimi" type="text" value="<?php echo $animal_name; ?>"> <?php echo $animal_name_error; ?><br><br>
+	<label for="looma_liik" >Looma liik</label><br>
+	<input id="looma_liik" name="looma_liik" type="text" value="<?php echo $animal_kind; ?>"> <?php echo $animal_kind_error; ?><br><br>
+	<label for="kuup2ev" >Kuupäev</label><br>
+	<input id="kuup2ev" name="kuup2ev" type="date" value="<?php echo $date; ?>"> <?php echo $date_error; ?><br><br>
+	<label for="probleem" >Probleemi kirjeldus</label><br>
+	<input id="probleem" name="probleem" type="text" value="<?php echo $problem; ?>"> <?php echo $problem_error; ?><br><br>
+	<input type="submit" name="register" value="Salvesta">
+</form>
--- functions.php
+++ functions.php
@@ -11,5 +11,39 @@
 	
 		//saadan ühenduse classi ja loon uue classi
 	$User = new User($mysqli);
+
 	
-	?>
+	function cleanInput($data) {
+  	  $data = trim($data);
+  	  $data = stripslashes($data);
+  	  $data = htmlspecialchars($data);
+  	  return $data;
+    }
+	
+	
+	
+	function registerOwner($owner_name, $animal_name, $animal_kind, $date, $problem){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO looma_omanikud (oid, nimi, o_looma_nimi) VALUES (?, ?, ?)");
+		$stmt->bind_param("iss", $id, $owner_name, $animal_name);
+		$stmt->close();
+		$mysqli->close();
+		
+	function registerAnimal($owner_name, $animal_name, $animal_kind, $date, $problem){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO loomad (lid, looma_nimi, omaniku_nimi, looma_liik, looma_kirjeldus) VALUES (?, ?, ?, ?, ?)");
+		$stmt->bind_param("issss", $id, $animal_name, $owner_name, $animal_kind, $problem);
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function registerDate($owner_name, $animal_name, $animal_kind, $date, $problem){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO vastuv6tuajad (aeg, looma_nimi) VALUES (?, ?)");
+		$stmt->bind_param("ss", $date, $animal_name);
+		$stmt->close();
+		$mysqli->close();
+	}
+
+
+?>
