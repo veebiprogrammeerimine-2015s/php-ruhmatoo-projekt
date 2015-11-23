5e711a0649a60835d842499a183124f15e5662b7
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-11-23 15:36:10 +0200

    more button stuff

--- footer.php
+++ footer.php
@@ -8,7 +8,9 @@
 	  <div class="col-xs-6 col-md-3">Telefoni nr</div>
 	  <div class="col-xs-6 col-md-3">info@ratemypro.ee</div>
 
-  
+  <ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="#">Back to top</a></li>
+  </ul>
   
   </div>
   
@@ -18,9 +20,7 @@
 	  <div class="col-xs-6 col-md-3">Olematu mnt 112b</div>
 	  <div class="col-xs-6 col-md-3">info@ratemypro.ee</div>
 
-  <ul class="nav nav-pills">
-  <li role="presentation" class="active"><a href="#">Back to top</a></li>
-  </ul>
+  
   </div>
 </div>
 
