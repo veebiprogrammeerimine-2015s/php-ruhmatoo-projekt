6b6c8cd7d6175dcfe493d0082176a323624dbdc4
Kristian Talviste <kristal@greeny.cs.tlu.ee>
2015-11-23 14:33:26 +0200

    index page

--- index.php
+++ index.php
@@ -1,5 +1,68 @@
-<?php
-	
-	echo "lahekristian"
+<?php 	require_once("header.php"); ?>
 
-?>
+<!-- navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Mikud</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    
+    <br><br><br><br>
+
+
+
+	<!-- Sisu -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-6">
+                <h1>Welcome to my site!</h1>
+            </div>
+            <div class="col-sm-offset-1 col-sm-3">
+            <form>
+              <div class="form-group">
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+              </div>
+              
+
+                <div class="row">
+                
+                    <div class="col-lg-8">
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-4 hidden-sm hidden-md">
+                        <button type="submit" class="btn btn-info btn-block">Login 1</button>
+                    </div>
+                    
+                    <div class="col-lg-4 hidden-lg hidden-xs pull-right">
+                        <button type="submit" class="btn btn-info">Login 2</button>
+                    </div>
+                    
+                </div>
+               
+            </form>
+            </div>
+                    
+        </div>
+    </div>
+	
+<?php	require_once("footer.php"); ?>
