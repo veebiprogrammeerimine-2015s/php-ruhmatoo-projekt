d8e512374f2d003b23515a15f6e1f8e65201116c
Helen Puhu <helepuh@greeny.cs.tlu.ee>
2015-11-25 13:14:25 +0200

    added style.css

--- login.php
+++ login.php
@@ -1,10 +1,6 @@
 <?php
 		
-<<<<<<< HEAD
 	// kõik funktsioonid, kus tegeleme AB'iga
-=======
-	// kÃµik funktsioonid, kus tegeleme AB'iga
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 	require_once("functions.php");
 	
 	//kui kasutaja on sisseloginud,
@@ -20,11 +16,7 @@
 	$password_error = "";
 	$create_email_error = "";
 	$create_password_error = "";
-<<<<<<< HEAD
   // muutujad väärtuste jaoks
-=======
-  // muutujad vÃ¤Ã¤rtuste jaoks
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 	$email = "";
 	$password = "";
 	$create_email = "";
@@ -35,7 +27,6 @@
 		// *********************
 		if(isset($_POST["login"])){
 			if ( empty($_POST["email"]) ) {
-<<<<<<< HEAD
 				$email_error = "See väli on kohustuslik";
 			}else{
 			// puhastame muutuja võimalikest üleliigsetest sümbolitest
@@ -49,41 +40,12 @@
 			// Kui oleme siia jõudnud, võime kasutaja sisse logida
 			if($password_error == "" && $email_error == ""){
 				echo "Võib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
-=======
-				$email_error = "See vÃ¤li on kohustuslik";
-			}else{
-			// puhastame muutuja vÃµimalikest Ã¼leliigsetest sÃ¼mbolitest
-				$email = cleanInput($_POST["email"]);
-			}
-			if ( empty($_POST["password"]) ) {
-				$password_error = "See vÃ¤li on kohustuslik";
-			}else{
-				$password = cleanInput($_POST["password"]);
-			}
-			// Kui oleme siia jÃµudnud, vÃµime kasutaja sisse logida
-			if($password_error == "" && $email_error == ""){
-				echo "VÃµib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 			
 				$hash = hash("sha512", $password);
 				
 				// kasutaja sisselogimise fn, failist functions.php
 				$login_response = $User->loginUser($email, $hash);
 				
-<<<<<<< HEAD
-				//kasutaja logis edukalt sisse 
-				if(isset($login_response->success)){
-					
-					//id, email
-					$_SESSION["logged_in_user_id"] = $login_response->user->id;
-					$_SESSION["logged_in_user_email"] = $login_response->user->email;
-					
-					//saadan sõnumi teise faili kasutades sessiooni
-					$_SESSION["login_success_message"] = $login_response->success->message;
-					header("Location: data.php");
-				}
-				
-=======
 				//kasutaja logis edukalt sisse
 				if(isset($login_response->success)){
 					
@@ -98,7 +60,6 @@
 				}
 				
 				
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 			}
 		} // login if end
 		// *********************
@@ -106,43 +67,25 @@
 		// *********************
 		if(isset($_POST["create"])){
 				if ( empty($_POST["create_email"]) ) {
-<<<<<<< HEAD
 					$create_email_error = "See väli on kohustuslik";
-=======
-					$create_email_error = "See vÃ¤li on kohustuslik";
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 				}else{
 					$create_email = cleanInput($_POST["create_email"]);
 				}
 				if ( empty($_POST["create_password"]) ) {
-<<<<<<< HEAD
 					$create_password_error = "See väli on kohustuslik";
 				} else {
 					if(strlen($_POST["create_password"]) < 8) {
 						$create_password_error = "Peab olema vähemalt 8 tähemärki pikk!";
-=======
-					$create_password_error = "See vÃ¤li on kohustuslik";
-				} else {
-					if(strlen($_POST["create_password"]) < 8) {
-						$create_password_error = "Peab olema vÃ¤hemalt 8 tÃ¤hemÃ¤rki pikk!";
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 					}else{
 						$create_password = cleanInput($_POST["create_password"]);
 					}
 				}
 				if(	$create_email_error == "" && $create_password_error == ""){
 					
-<<<<<<< HEAD
 					// räsi paroolist, mille salvestame ab'i
 					$hash = hash("sha512", $create_password);
 					
 					echo "Võib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password." ja räsi on ".$hash;
-=======
-					// rÃ¤si paroolist, mille salvestame ab'i
-					$hash = hash("sha512", $create_password);
-					
-					echo "VÃµib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password." ja rÃ¤si on ".$hash;
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 					
 					// kasutaja loomise fn, failist functions.php,
 					// saadame kaasa muutujad
@@ -153,16 +96,6 @@
 				}
 		} // create if end
 	}
-<<<<<<< HEAD
-  // funktsioon, mis eemaldab kõikvõimaliku üleliigse tekstist
-  function cleanInput($data) {
-  	$data = trim($data);
-  	$data = stripslashes($data);
-  	$data = htmlspecialchars($data);
-  	return $data;
-  }
-=======
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 	
 	
 	
@@ -170,21 +103,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-=======
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
-  <title>Login</title>
+  <title>Log in</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+ <div class="login-form">
   <h2>Log in</h2>
     <?php if(isset($login_response->error)): ?>
   
@@ -199,19 +122,11 @@
 	</p>
 	
   <?php endif; ?>
-<<<<<<< HEAD
-  <div class="Login">
-=======
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
-  	<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?><br><br>
+	<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?><br><br>
   	<input name="password" type="password" placeholder="Parool" value="<?php echo $password; ?>"> <?php echo $password_error; ?><br><br>
   	<input type="submit" name="login" value="Log in">
   </form>
-<<<<<<< HEAD
-  </div>
-=======
->>>>>>> 0bdb3f68132879efa65109c929fa42249c11291d
 
   <h2>Create user</h2>
   
@@ -235,5 +150,6 @@
   	<input name="create_password" type="password" placeholder="Parool"> <?php echo $create_password_error; ?> <br><br>
   	<input type="submit" name="create" value="Create user">
   </form>
+   </div>
 <body>
 <html>
