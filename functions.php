a41f52823f2c6136afc4437f102caf9a5bb2b0b8
Hendrik Seemen <hendrik7@greeny.cs.tlu.ee>
2015-11-25 12:33:51 +0200

    added more files

---
+++ functions.php
@@ -0,0 +1,163 @@
+<?php 
+	
+	require_once("../config.php");
+	$database = "if15_hendrik7";
+	
+	session_start();
+	
+	
+	//function createUser($create_email, $hash){
+	function createUser($create_email, $hash, $username, $fullname){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		echo $mysqli->error;
+		$stmt = $mysqli->prepare("INSERT INTO users (email, password, username, fullname) VALUES (?, ?, ?, ?)");
+				
+		$stmt->bind_param("ssss", $create_email, $hash, $username, $fullname);
+		$stmt->execute();
+		echo $stmt->error;
+		$stmt->close();
+		
+		
+	}
+	
+	
+	function loginUser($email, $hash){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, email FROM users WHERE email=? AND password=?");
+		$stmt->bind_param("ss", $email, $hash);
+				
+		//muutujad tulemustele
+		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt->execute();
+				
+			//kontrollin kas tulemusi leiti
+			if($stmt->fetch()){
+			//ab'i oli midagi
+			echo "Email ja parool õiged, kasutaja id=".$id_from_db;
+					
+					//tekitan sessiooni muutujad
+					$_SESSION["logged_in_user_id"] = $id_from_db;
+					$_SESSION["logged_in_user_email"] = $email_from_db;
+					
+					//suunan data.php lehele
+					header("Location: data.php");
+					
+			}else{
+			//ei leidnud
+				echo "wrong credentails!";
+					
+			}
+				
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	function postMedia($title, $media){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO user_content (user_id, title, media) VALUES (?, ?, ?)");
+		echo $mysqli->error;
+		$stmt->bind_param("iss",$_SESSION["logged_in_user_id"], $title, $media);
+		$stmt->execute();
+		echo $stmt->error;
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	function getPosts($keyword=""){
+		
+		$search = "%%";
+		
+		if($keyword == ""){
+			//ei otsi midagi
+			echo "Ei otsi";
+		}else{
+			//otsin
+			echo "Otsin ";
+			$search= "%".$keyword."%";
+		}
+		
+		
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, user_id, title, media from user_content WHERE deleted IS NULL AND
+		(title LIKE ?)");
+		echo $mysqli->error;
+		$stmt->bind_param("s", $search);
+		$stmt->bind_result($id, $user_id_from_database, $title, $media);
+		$stmt->execute();
+		
+		// tekitan tühja massiivi, kus edaspidi hoian objekte
+		$content_array = array();
+		
+		//tee midagi seni, kuni saame ab'ist ühe rea andmeid
+		while($stmt->fetch()){
+			// seda siin sees tehakse 
+			// nii mitu korda kui on ridu
+			// tekitan objekti, kus hakkan hoidma väärtusi
+			$content = new StdClass();
+			$content->id = $id;
+			$content->title = $title;
+			$content->media = $media;
+			$content->user_id = $user_id_from_database;
+			
+			
+			//lisan massiivi ühe rea juurde
+			array_push($content_array, $content);
+			//var dump ütleb muutuja tüübi ja sisu
+			//echo "<pre>";
+			//var_dump($car_array);
+			//echo "</pre><br>";
+		}
+		
+		//tagastan massiivi, kus kõik read sees
+		return $content_array;
+		
+		
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	
+	function deleteContent($id){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("UPDATE user_content SET deleted=NOW() WHERE id=? AND user_id=?");
+		$stmt->bind_param("ii", $id, $_SESSION["logged_in_user_id"]);
+		if($stmt->execute()){
+			// sai kustutatud
+			// kustutame aadressirea tühjaks
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		
+		
+	}
+	
+	function updateContent($id, $title, $media){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("UPDATE user_content SET title=?, media=? WHERE id=? AND user_id=?");
+		$stmt->bind_param("ssii", $title, $media, $id, $_SESSION["logged_in_user_id"]);
+		if($stmt->execute()){
+			// sai uuendatud
+			// kustutame aadressirea tühjaks
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	
+	
+	
+?>
---
+++ login.php
@@ -0,0 +1,177 @@
+<?php
+	
+	// LOGIN.PHP
+	
+	// loon andmebaasi ühenduse
+	require_once("functions.php");
+	
+	if(isset($_SESSION["logged_in_user_id"])){
+		header("Location: data.php");
+	}
+	
+	
+	// muutujad errorite jaoks
+	$name_error = "";
+	$email_error = "";
+	$password_error = "";
+	$create_password_error = "";
+	$create_email_error = "";
+	
+	//muutujad andmebaasi väärtuste jaoks
+	
+	$email = "";
+	$username = "";
+	$fullname ="";
+	$create_email="";
+	
+	//kontrollime, et keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		// LOGI SISSE
+		
+		if (isset($_POST["login"])) {
+			
+			//kontrollin, et e-post ei ole tühi
+			if (empty($_POST["email"])) {
+				$email_error = "See väli on kohustuslik";
+				
+			}else{
+				$email = test_input($_POST["email"]);
+			}
+				
+			//kontrollin, et parool ei ole tühi
+			if (empty($_POST["password"])) {
+				$password_error = "See väli on kohustuslik";
+			} else {
+				$password = test_input($_POST["password"]);				
+			}
+			
+			// võib sisse logida
+			
+			if($password_error == "" && $email_error == ""){
+				echo "Võib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
+				
+				$hash = hash("sha512", $password);
+				
+				loginUser($email, $hash);
+				
+			}
+			
+		}
+		
+		// LOO KASUTAJA
+		
+		elseif (isset($_POST["create"])) //registration field errors
+		{
+			
+			
+			if (empty($_POST["username"])) {
+				$name_error = "See väli on kohustuslik";	
+			} else {
+				$username = test_input($_POST["username"]);
+			}
+			
+			
+			
+			
+			if (empty($_POST["fullname"])) {
+				$name_error = "See väli on kohustuslik";	
+			} else {
+				$fullname = test_input($_POST["fullname"]);
+			}
+			
+			
+			
+			
+			
+			if (empty($_POST["create_email"])) {
+				$create_email_error = "See väli on kohustuslik";
+				
+			}else{
+				
+				$create_email = test_input($_POST["create_email"]);
+			}
+			
+			
+			
+			if (empty($_POST["create_password"])) {
+				$create_password_error = "See väli on kohustuslik";
+			
+				
+			}else{
+				if(strlen($_POST["create_password"]) <8) {
+					
+					$create_password_error = "Peab olema vähemalt 8 tähemärki pikk";
+				
+				}else{
+					$create_password = test_input($_POST["create_password"]);
+				}
+				
+			}
+			
+			if ($name_error == ""){
+				echo "salvestasin andmebaasi ".$username;
+			}
+			
+			if(	$create_email_error == "" && $create_password_error == "" && $name_error == ""){
+				
+				$hash = hash("sha512", $create_password);
+				echo "Kasutaja on loodud! Registreeritud e-mail on ".$create_email.", kasutajanimi on ".$username." ja parool on ".$create_password."ja räsi on ".$hash;
+				createUser($create_email, $hash, $username, $fullname);
+				
+			}
+			
+		}
+		
+	}	
+	
+	function test_input($data) {
+		// võtab ära tühikud, enterid, tabid
+		$data = trim($data);
+		//tagurpidi kaldkriipsud
+		$data = stripslashes($data);
+		//teeb html'i tekstiks
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+	
+		
+	
+?>
+<?php
+	$page_title = "Kasutaja leht";
+	$page_file_name = "login.php"
+?>	
+
+<?php require_once("header.php"); ?>
+<html>
+<head>
+	<title>Kasutaja leht</title>
+</head>
+<body>
+	<FONT FACE="arial">
+	
+	<h2>Logi sisse</h2>
+	
+	<form action="login.php" method="post">
+		<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?> <br><br>
+		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
+		<input type="submit" name="login" value="Logi sisse">
+	</form>	
+		
+	<h2>Loo kasutaja</h2>
+	
+	<form action="login.php" method="post">
+		<input name="fullname" type="text" placeholder="Täisnimi" value="<?php echo $fullname; ?>"> <?php echo $name_error; ?> <br><br>
+		<input name="username" type="name" placeholder="Kasutajanimi" value="<?php echo $username; ?>"> <?php echo $name_error; ?> <br><br>
+		<input name="create_email" type="email" placeholder="E-post" value="<?php echo $create_email; ?>"> <?php echo $create_email_error; ?> <br><br>
+		<input name="create_password" type="password" placeholder="Parool"> <?php echo $create_password_error; ?> <br><br>
+		<input name="create" type="submit" value="Sisesta">
+	</form>	
+	
+	</FONT>
+	
+</body>
+
+</html>
+<?php require_once("footer.php"); ?>
