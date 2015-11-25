0f3ccfa7f5a7dded363194dd8652dfe27e8e15cc
Raoul Kirsima <raoukir@greeny.cs.tlu.ee>
2015-11-25 13:17:36 +0200

    added create user function

--- login.php
+++ login.php
@@ -54,8 +54,8 @@
 						</div>
 					</div>
 					<div class="col-md-4">
-					<button type="submit" class="btn btn-info hidden-xs ">Login 1 </button>
-					<button type="submit" class="btn btn-info btn-block visible-xs">Login 2 </button>
+					<button type="submit" class="btn btn-info hidden-xs ">Login</button>
+					<button type="submit" class="btn btn-info btn-block visible-xs">Login</button>
 					</div>
 					
 				
@@ -63,6 +63,32 @@
 			  
 			</form>
 			
+			<br><br>
+			
+			
+			<form>
+				<h1>CREATE NEW USER</h1>
+			  <div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			  </div>
+			  
+				<div class="row">
+					<div class="col-md-8">
+						<div class="form-group">
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						<input type="text" class="form-control" id="exampleInputFirstName" placeholder="First name">
+						<input type="text" class="form-control" id="exampleInputLastName" placeholder="Last name">
+						</div>
+					</div>
+					<div class="col-md-4">
+					<button type="submit" class="btn btn-info hidden-xs ">Login</button>
+					<button type="submit" class="btn btn-info btn-block visible-xs">Login</button>
+					</div>
+					
+				
+			  </div>
+			  
+			</form>
 			
 		</div>
 		
