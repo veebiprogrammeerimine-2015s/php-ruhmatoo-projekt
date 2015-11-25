1735efbdce33ad4557831d1d826bcd32f0ef2e6e
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2015-11-25 13:29:52 +0200

    modified footer

--- main.php
+++ main.php
@@ -9,7 +9,7 @@
 		<div class="col-sm-6 col-sm-pull-3">
 			<div class="jumbotron">
 				<div class="container">
-					<h1>Teade</h1>
+					<h2>Teade</h2>
 					<p>
 					<a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
 					Loe teadet
