e47291e0afddb31a94afd5d8b130b43f013f584b
Hendrik Seemen <hendrik7@greeny.cs.tlu.ee>
2015-12-02 12:19:56 +0200

    added classes

---
+++ classes/Series.class.php
@@ -0,0 +1,131 @@
+<?php
+class Series {
+	
+	private $connection;
+	private $user_id;
+	
+	//kui tekitan new, siis käivitatakse see funktsioon
+	
+	function __construct($mysqli, $user_id_from_session){
+		
+		$this->connection = $mysqli;
+		$this->user_id = $user_id_from_session;
+		
+		echo "Huvialade haldus käivitatud, kasutaja=".$this->user_id;
+		
+	}
+	
+	function addSeries($add_series){
+		
+		$response = new StdClass();
+		
+		
+		$stmt = $this->connection->prepare("SELECT id FROM series WHERE name=?");
+		$stmt->bind_param("s", $add_interest);
+		$stmt->bind_result($id);
+		$stmt->execute();
+		
+		if($stmt->fetch()){
+			
+			
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "Seriaal <strong>".$add_interest."</strong> on juba olemas!";
+			
+			$response->error = $error;
+			
+			return $response;
+			
+		}
+		
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO series (name, season, description, picture) VALUES (?, ?, ?, ?)");
+				
+		
+		$stmt->bind_param("ssss", $name, $season, $description, $picture);
+		if($stmt->execute()) {
+			
+			$success = new StdClass();
+			$success->message = "Seriaal edukalt sisestatud!";
+			$response->success = $success;
+			
+			
+		}else {
+			
+			//midagi läks katki
+			$error = new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi läks katki!";
+			
+			$response->error = $error;
+			
+			
+		}
+		
+		
+		$stmt->close();
+		
+		return $response;
+		
+		
+	}
+	
+	
+	function addUserList($add_list){
+		
+		$response = new StdClass();
+		
+		//kas sellel kasutajal on see list
+		$stmt = $this->connection->prepare("SELECT id FROM user_list WHERE user_id=? AND name=?");
+		$stmt->bind_param("is", $this->user_id, $add_list);
+		$stmt->bind_result($id);
+		$stmt->execute();
+		
+		if($stmt->fetch()){
+			
+			
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "Sellise nimega list on sinul juba olemas!";
+			
+			$response->error = $error;
+			
+			return $response;
+			
+		}
+		
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO user_list (user_id, name) VALUES (?,?)");
+				
+		
+		$stmt->bind_param("is", $this->user_id, $new_interest_id);
+		if($stmt->execute()) {
+			
+			$success = new StdClass();
+			$success->message = "Huviala edukalt sisestatud!";
+			$response->success = $success;
+			
+			
+		}else {
+			
+			//midagi läks katki
+			$error = new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi läks katki!";
+			
+			$response->error = $error;
+			
+			
+		}
+		
+		
+		$stmt->close();
+		
+		return $response;
+		
+		
+	}
+	
+} ?>
---
+++ pages/data.php
@@ -0,0 +1,106 @@
+<?php require_once("header.php"); ?>
+
+<?php
+	require_once("functions.php");
+	//data
+	//siia pääseb ligi sisseloginud kasutaja
+	//kui kasutaja on sisse loginud
+	//siis suunan data.php lehele
+	if(!isset($_SESSION["logged_in_user_id"])){
+		header("Location: login.php");
+	}
+	
+	//kasutaja tahab välja logida
+	if(isset($_GET["logout"])){
+		//aadressireal on olemas muutuja logout
+		
+		//kusutame kõik session muutujad ja peatame sessiooni
+		session_destroy();
+		
+		header("Location: login.php");
+	}
+	
+	$title = $season = $intro = $photo = "";
+	$title_error = $season_error = $intro_error = $photo_error = "";
+	
+	
+	if(isset($_POST["postseason"])){
+		echo "vajutati nuppu";
+		if ( empty($_POST["title"]) ) {
+				$title_error = "See väli on kohustuslik";
+			}else{
+				$title = cleanInput($_POST["title"]);
+			}
+
+			if ( empty($_POST["season"]) ) {
+				$season_error = "See väli on kohustuslik";
+			} else {
+				
+				$season = cleanInput($_POST["season"]);
+				
+			}
+			if ( empty($_POST["intro"]) ) {
+				$intro_error = "See väli on kohustuslik";
+			} else {
+				
+				$intro = cleanInput($_POST["intro"]);
+				
+			}
+			if ( empty($_POST["photo"]) ) {
+				$photo_error = "See väli on kohustuslik";
+			} else {
+				
+				$intro = cleanInput($_POST["photo"]);
+				
+			}
+		if(	$title_error == "" && $season_error == "" && $intro_error == "" && $photo_error == ""){
+			
+			echo "Sisestatud!";
+				
+				
+				postseason($title, $season, $intro, $photo);
+			
+		}
+	}
+	function cleanInput($data) {
+  	$data = trim($data);
+  	$data = stripslashes($data);
+  	$data = htmlspecialchars($data);
+  	return $data;
+  }
+	
+	
+	
+?>
+
+
+
+		
+			
+	
+
+	
+
+	
+<p>
+	Tere, <?=$_SESSION["logged_in_user_id"];?>
+	<a href="?logout=1">Logi välja</a>
+</p>
+
+
+<h2>Lisa uus seriaal</h2>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<label for="title" >Nimi</label><br>
+  	<input name="title" id="title" type="text"  value="<?php echo $title; ?>"> <?php echo $title_error; ?><br><br>
+	<label for="season" >Hooaeg</label><br>
+  	<input name="season" type="text"  value="<?php echo $season; ?>"> <?php echo $season_error; ?><br><br>
+	<label for="intro" >Tutvustus</label><br>
+  	<textarea name="intro" rows="10" cols="100"><?php echo $intro; ?></textarea> <?php echo $intro_error; ?><br><br>
+	<label for="photo" >Lisa seriaali pilt</label><br>
+  	<input name="photo" type="text"  value="<?php echo $photo; ?>"> <?php echo $photo_error; ?><br><br>	
+  	<input type="submit" name="postseason" value="Salvesta">
+  </form>
+  
+  <a href="table.php">Vaata/Muuda postitusi</a>
+  
+  <?php require_once("footer.php"); ?>
