38a34c1b95af0d6055ef806832c17e102eb476c3
ardo <np3799@a180.radicenter.eu>
2015-12-03 18:23:58 +0200

    added book now post parameterst

--- book-now.php
+++ book-now.php
@@ -14,7 +14,7 @@
         $timeavailableid = $_GET["timeavailableid"];
     }
 	$getTimeInfo = $AvailableTimeDetails->getFreeTimesDetails($timeavailableid);
-	var_dump($getTimeInfo);
+	//var_dump($getTimeInfo);
 	$hospidal_name = $getTimeInfo[0]->hospidal_name;
 	$dr_name = $getTimeInfo[0]->dr_name;
 	$area = $getTimeInfo[0]->area;
@@ -28,7 +28,23 @@
 	//var_dump($getDrDayTimes);
 	
 	
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		if(isset($_POST["book-now"])){
+					$selected_time = ($_POST["selectedavailabletime"]);
+					$problem_description = ($_POST["problemdescrpt"]);
+					echo $selected_time;
+					echo $problem_description;
+		}
+		
+	
+	}
+	
 ?>
+
+<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
+<?php echo htmlspecialchars($_SERVER["QUERY_STRING"]);?>
+
 <div class="container">
 	<div class="row">
 	<H1> Broneeringu detailandmed </h1>
@@ -36,8 +52,9 @@
   			<h2>Kelle juurde </h2>
   			<label for="comment">Arsti nimi:</label>  <?php echo $dr_name ?>
   			<div class="form-group">
+  		<form action="<?php // echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
   				<label for="comment">Sisesta oma mure siia:</label>
-  			<textarea class="form-control" rows="5" id="problem-descrition"></textarea>
+  			<textarea class="form-control" rows="5" name="problemdescrpt" id="problem-descrition"></textarea>
 			</div> 
 		</div>
 		<div class="col-md-3 ">
@@ -45,7 +62,8 @@
   			<label for="comment">Kuupäev:</label> <?php echo $date_appoitmnt; ?> <br>
   			<label for="comment">Vali sobiv aeg:</label>
   			<?php echo $AvailableTimeDetails->build_table($getDrDayTimes); ?>
-  			
+  			<input type="submit" name="book-now" value="Broneeri valitud aeg">
+  			</form>	
   			
   			
 		</div> 
@@ -53,10 +71,9 @@
   			<h2>Asutus </h2>
   			<label for="comment">Asutuse nimi: </label>  <?php echo $hospidal_name ?></br>
   			<label for="comment">Aadress: </label> <?php echo $address.", ". $area.", ".$city;  ?></br>
-  			
-  			
-  
-		</div> 				
+		</div> 
+	
+				
   	</div>
 </div>
 
