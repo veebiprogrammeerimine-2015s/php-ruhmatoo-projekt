da01b43b4ba5a963aa42acbf0c516e6c28a5f7a3
Hendrik Seemen <hendrikseemen@users.noreply.github.com>
2015-12-07 19:56:38 +0200

    changes to header

--- header.php
+++ header.php
@@ -29,9 +29,24 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">My Series <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">All Series</a></li>
+        <li><a href="home.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="data.php">My Lists</a></li>
+		
       </ul>
+	   <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+	  <ul class="nav navbar-nav navbar-right">
+		  <li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Log in</a></li>
+				<li><a href="#">Create User</a></li>
+			</ul>
+		
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
