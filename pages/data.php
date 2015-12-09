57289da891a7553ed6d61df65472fad6b148d488
Hendrik Seemen <hendrikseemen@users.noreply.github.com>
2015-12-09 13:48:07 +0200

    added and fixed functions

--- pages/data.php
+++ pages/data.php
@@ -9,6 +9,8 @@
 		header("Location: login.php");
 	}
 	
+	$Series = new Series($mysqli, $_SESSION["logged_in_user_id"]);
+	
 	//kasutaja tahab välja logida
 	if(isset($_GET["logout"])){
 		//aadressireal on olemas muutuja logout
@@ -49,7 +51,7 @@
 				$picture_error = "See väli on kohustuslik";
 			} else {
 				
-				$description = cleanInput($_POST["picture"]);
+				$picture = cleanInput($_POST["picture"]);
 				
 			}
 		if(	$title_error == "" && $season_error == "" && $description_error == "" && $picture_error == ""){
@@ -57,8 +59,22 @@
 			echo "Sisestatud!";
 				
 				
-				addSeries($title, $season, $description, $picture);
+				$add_response = $Series->addSeries($title, $season, $description, $picture);
+				
+		}
+	}
+	
+	if(isset($_POST["createList"])){
+		if ( empty($_POST["name"]) ) {
+					$name_error = "Field is empty";
+				}else{
+					$name = cleanInput($_POST["name"]);
+				}
+		if($name_error == ""){
 			
+			echo "List created";
+			
+			$add_list_response = $Series->createList($name);
 		}
 	}
 	function cleanInput($data) {
@@ -80,7 +96,8 @@
 			Tere, <?=$_SESSION["name"];?>
 			<a href="?logout=1">Logi välja</a>
 		</p>
-
+<?php var_dump($add_response);?>
+<?php var_dump($add_list_response);?>
 
 		<h2>Add new series</h2>
 		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
@@ -104,4 +121,49 @@
 		  </form>
 	</div>
 </div>
+
+<form method="post">
+    <input name="name" type="text">
+    <input type="submit" name="createList" value="Submit">
+</form>
+<?php
+function dropdown(){
+		
+		$html = "";
+		
+		$html .= '<select name="new_dd_selection">';
+		//$html .= '<option>1</option>';
+		//$html .= '<option>2</option>';
+		//$html .= '<option>3</option>';
+		//$html .= '<option selected>3</option>';
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, name FROM user_list where user_id=?");
+		
+		$stmt->bind_param("i", $_SESSION["logged_in_user_id"]);
+		$stmt->bind_result($id, $name);
+		$stmt->execute();
+		
+		while($stmt->fetch()){
+			$html .= '<option value="'.$id.'">'.$name.'</option>';
+			
+		}
+		
+		
+		$html .= '</select>';
+		
+		return $html;
+		
+	}
+
+?>
+
+<a href="?add=1">Lisa</a>
+<?php if(isset($_GET["add"])){ ?>
+<form method="post">
+    <input type="hidden" name="add" value="<?=$_GET["add"];?>" type="text">
+	<?php echo dropdown(); ?>
+    <input type="submit" name="createList" value="Submit">
+</form>
+<?php } ?>
+
   <?php require_once("../footer.php"); ?>
