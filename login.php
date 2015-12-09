67ce2f7573424bbf89ca3a2418dbd6e676867a23
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2015-12-09 11:19:59 +0200

    tiitelleht suunamine

--- login.php
+++ login.php
@@ -14,14 +14,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Ruslan</a>
+      <a class="navbar-brand" href="#">Eesti post</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li class="active"><a href="#">.<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">.</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
