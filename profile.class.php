37ab6227c9e60a8b618d461324ae17a4964054d8
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-12-18 13:59:19 +0200

    #12 TABid, profiili esialgne kuvand

--- profile.class.php
+++ profile.class.php
@@ -1,16 +1,16 @@
 <?php
 class Profile {
 	private $connection;
-    
+
     function __construct($mysqli){
         $this->connection = $mysqli;
     }
-	
+
 	/*Tööotsija*/
-	
-	
+
+
 	/*Tööotsija lõpp*/
-	
+
 	/*Tööpakkuja*/
 	function createCompany($create_company, $create_email, $create_number){
 		$response = new StdClass();
@@ -19,14 +19,14 @@ class Profile {
 		$stmt->bind_param("s", $create_company);
 		$stmt->bind_result($name);
 		$stmt->execute();
-		
+
 		if($stmt->fetch()) {
-			
+
 			$error = new StdClass();
 			$error->id = 0;
 			$error->message = "Selline ettevõte on juba kasutusel!";
 			$response->error = $error;
-			
+
 			return $response;
 		}
 
@@ -36,7 +36,7 @@ class Profile {
 			$success = new StdClass();
 			$success->message = "Ettevõte sisestatud!";
 			$response->success = $success;
-			
+
 		} else {
 			$error = new StdClass();
 			$error->id = 1;
@@ -47,24 +47,24 @@ class Profile {
         header("Location: profile.php");
 		exit();
 		return $response;
-        
+
     }
-	
+
 	function editCompany($name, $email, $number, $user) {
 		$stmt = $this->connection->prepare("SET foreign_key_checks = 0");
 		$stmt->execute();
-		
+
 		$stmt = $this->connection->prepare("UPDATE job_company SET name=?, email=?, number=? WHERE name=?");
 		$stmt->bind_param("ssis", $name, $email, $number, $user);
 		$stmt->execute();
-		
+
 		$stmt = $this->connection->prepare("UPDATE job_offers SET job_offers.company=? WHERE job_offers.company = ?");
 		$stmt->bind_param("ss", $name, $user);
 		$stmt->execute();
-		
+
 		$stmt = $this->connection->prepare("SET foreign_key_checks = 1");
 		$stmt->execute();
-		
+
 		#$stmt = $this->connection->prepare("SET foreign_key_checks = 0;
 		#									UPDATE job_company JOIN job_offers ON job_offers.company = job_company.name SET job_offers.company='$name', job_company.name='$name', job_company.email='$email', job_company.number=$number WHERE job_company.user_id = $user AND job_offers.user_id = $user;
 		#									SET foreign_key_checks = 1;");
@@ -74,7 +74,7 @@ class Profile {
 		$stmt->close();
 		header ("Location: profile.php");
 	}
-	
+
 	function companyCheck($user) {
 		$stmt = $this->connection->prepare("SELECT user_id, email, number, name FROM job_company WHERE user_id = ?");
 		$stmt->bind_param("i", $user);
@@ -95,9 +95,9 @@ class Profile {
 		return ($job);
 		$stmt->close();
 	}
-		
-	
-	
+
+
+
 	/*Tööpakkuja lõpp*/
 }
 ?>
--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@
 	require_once("functions.php");
 ?>
 
-<?php 
+<?php
 	if(!isset($_SESSION['logged_in_user_id'])) {
 	header("Location: noaccess.php");
 	exit();
@@ -15,6 +15,16 @@
 ?>
 
 <?php
+	##########################
+	### SEARCHER VARIABLES ###
+	##########################
+
+
+
+
+	##########################
+	### EMPLOYER VARIABLES ###
+	##########################
 	$create_company = $create_email = $create_number = "";
 	$create_company_error = $create_email_error = $create_number_error = "";
 	$job_company_error = $job_email_error = $job_number_error = "";
@@ -24,25 +34,29 @@
 	$response = "";
 	$company_check = $Profile->companyCheck($_SESSION['logged_in_user_id']);
 
-	if( $_SERVER["REQUEST_METHOD"] == "POST") {
-		if(isset($_POST["add_company"])){
-			if (empty($_POST["create_company"]) ) {
-				$create_company_error = "See väli on kohustuslik";
-			}else{
-				$create_company = cleanInput($_POST["create_company"]);
-			}
-			if (empty($_POST["create_email"]) ) {
-				$create_email_error = "See väli on kohustuslik";
-			}else{
-				$create_email = cleanInput($_POST["create_email"]);
-			}
-			if (empty($_POST["create_number"]) ) {
-				$create_number_error = "See väli on kohustuslik";
-			}else{
-				$create_number = cleanInput($_POST["create_number"]);
-			}
-			if($create_company_error == "" && $create_email_error == "" && $create_number_error == ""){
-				$response = $Profile->createCompany($create_company, $create_email, $create_number);
+	if(isset($_SESSION['logged_in_user_id'])) {
+		if($_SESSION['logged_in_user_group'] != 1) {
+			if( $_SERVER["REQUEST_METHOD"] == "POST") {
+				if(isset($_POST["add_company"])){
+					if (empty($_POST["create_company"]) ) {
+						$create_company_error = "See väli on kohustuslik";
+					}else{
+						$create_company = cleanInput($_POST["create_company"]);
+					}
+					if (empty($_POST["create_email"]) ) {
+						$create_email_error = "See väli on kohustuslik";
+					}else{
+						$create_email = cleanInput($_POST["create_email"]);
+					}
+					if (empty($_POST["create_number"]) ) {
+						$create_number_error = "See väli on kohustuslik";
+					}else{
+						$create_number = cleanInput($_POST["create_number"]);
+					}
+					if($create_company_error == "" && $create_email_error == "" && $create_number_error == ""){
+						$response = $Profile->createCompany($create_company, $create_email, $create_number);
+					}
+				}
 			}
 		}
 	}
@@ -54,34 +68,123 @@
 			} else {
 				$job_company1 = cleaninput($_POST["job_company1"]);
 			}
-			
+
 			if (empty($_POST["job_email1"])) {
 				$job_email_error1 = "See väli on kohustuslik!";
 			} else {
 				$job_email1 = cleaninput($_POST["job_email1"]);
 			}
-			
+
 			if (empty($_POST["job_number1"])) {
 				$job_number_error1 = "See väli on kohustuslik!";
 			} else {
 				$job_number1 = cleaninput($_POST["job_number1"]);
 			}
-			
+
 			if ($job_company_error1 == "" && $job_email_error1 == "" && $job_number_error1 == "") {
 				$response = $Profile->editCompany($job_company1, $job_email1, $job_number1, $old_name);
 			}
-			
+
 		}
 	}
 ?>
-<!-- 
+<!--
 ########################
 ### SEARCHER PROFIIL ###
 ########################
 -->
 <?php if($_SESSION['logged_in_user_group'] == 1):?>
-Otsija profiil
+<div class="row">
+	<div class="col-xs-12">
+	<div class="col-xs-12 col-sm-2">
+	<h2>Profiil</h2>
+	</div>
+	<div class="col-xs-12 col-sm-10">
+	<ul class="nav nav-tabs pull-right" role="tablist">
+	<li role="presentation" class="active"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Konto andmed</a></li>
+	<li role="presentation"><a href="#password" aria-controls="password" role="tab" data-toggle="tab">Muuda parooli</a></li>
+	<li role="presentation"><a href="#resumes" aria-controls="resumes" role="tab" data-toggle="tab">Konto CVd</a></li>
+	</ul>
+</div>
+</div>
+	<div class="col-xs-12">
+	<!-- Tab panes -->
+	<div class="tab-content">
+		<div role="tabpanel" class="tab-pane active" id="profile">
+			<div class="col-xs-12 col-sm-4">
+				<h3>Info</h3>
+				<pre class="pre-scrollable">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare sit amet erat id convallis. In hac habitasse platea dictumst. Sed a mauris sodales, tincidunt sapien non, hendrerit enim. Suspendisse potenti. Phasellus ut dui scelerisque, ultrices ex sed, fringilla dui. Ut fermentum enim sit amet sapien tristique, quis convallis nibh dapibus. Cras accumsan massa a augue elementum facilisis. Aenean dictum mauris ut erat rutrum faucibus. Praesent ac sollicitudin eros.
 
+Quisque rutrum egestas sem at luctus. Etiam quis magna mollis, hendrerit ex a, facilisis neque. Donec sit amet hendrerit erat. Morbi maximus egestas massa. In diam metus, molestie a blandit non, lobortis eu purus. Mauris id sapien sit amet nibh auctor luctus. Curabitur pretium mauris id ullamcorper blandit. Donec non interdum ligula. Cras sit amet magna dui.
+				</pre>
+			</div>
+
+			<div class="form-horizontal col-xs-12 col-sm-8">
+
+				<h3>Konto andmed</h3>
+
+				<table class="table table-striped table-bordered">
+					<tr>
+						<td><label> Kasutajanimi </label></td>
+						<td><?=$_SESSION['logged_in_user_email'];?></td>
+					</tr>
+					<tr>
+						<td><label> Konto loodud </label></td>
+						<td><?=date('d.m.Y');?></td>
+					</tr>
+					<tr>
+						<td><label> Viimane külastatus </label></td>
+						<td><?="Puudub";?></td>
+					</tr>
+					<tr>
+						<td><label> Viimane muudatus profiilis </label></td>
+						<td><?="Puudub";?></td>
+					</tr>
+					<tr>
+						<td><label> CVde arv </label></td>
+						<td><?="Puudub";?></td>
+					</tr>
+				</table>
+
+			</div>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="password">
+			<div class="col-xs-12 col-sm-4">
+				<h3>Info</h3>
+				<pre class="pre-scrollable">
+PAROOLI KIRJELDUS TULEB SIIA ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare sit amet erat id convallis. In hac habitasse platea dictumst. Sed a mauris sodales, tincidunt sapien non, hendrerit enim. Suspendisse potenti. Phasellus ut dui scelerisque, ultrices ex sed, fringilla dui. Ut fermentum enim sit amet sapien tristique, quis convallis nibh dapibus. Cras accumsan massa a augue elementum facilisis. Aenean dictum mauris ut erat rutrum faucibus. Praesent ac sollicitudin eros.
+
+Quisque rutrum egestas sem at luctus. Etiam quis magna mollis, hendrerit ex a, facilisis neque. Donec sit amet hendrerit erat. Morbi maximus egestas massa. In diam metus, molestie a blandit non, lobortis eu purus. Mauris id sapien sit amet nibh auctor luctus. Curabitur pretium mauris id ullamcorper blandit. Donec non interdum ligula. Cras sit amet magna dui.
+				</pre>
+			</div>
+
+			<div class="form-horizontal col-xs-12 col-sm-8">
+				<h3>Muuda parooli</h3>
+				PASS
+			</div>
+
+		</div>
+
+		<div role="tabpanel" class="tab-pane" id="resumes">
+			<div class="col-xs-12 col-sm-4">
+				<h3>Info</h3>
+				<pre class="pre-scrollable">
+CVDE KIRJELDUS TULEB KA SIIA ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare sit amet erat id convallis. In hac habitasse platea dictumst. Sed a mauris sodales, tincidunt sapien non, hendrerit enim. Suspendisse potenti. Phasellus ut dui scelerisque, ultrices ex sed, fringilla dui. Ut fermentum enim sit amet sapien tristique, quis convallis nibh dapibus. Cras accumsan massa a augue elementum facilisis. Aenean dictum mauris ut erat rutrum faucibus. Praesent ac sollicitudin eros.
+
+Quisque rutrum egestas sem at luctus. Etiam quis magna mollis, hendrerit ex a, facilisis neque. Donec sit amet hendrerit erat. Morbi maximus egestas massa. In diam metus, molestie a blandit non, lobortis eu purus. Mauris id sapien sit amet nibh auctor luctus. Curabitur pretium mauris id ullamcorper blandit. Donec non interdum ligula. Cras sit amet magna dui.
+				</pre>
+			</div>
+
+			<div class="form-horizontal col-xs-12 col-sm-8">
+				<h3>Minu CVd</h3>
+				CVd
+			</div>
+		</div>
+
+		</div>
+	</div>
+</div>
 <!--
 ########################
 ### EMPLOYER PROFILE ###
