dacc3e00e9e6e9d4d0169b7f61712cad9a4bdc49
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2016-01-05 15:15:26 +0200

    esimesed edusammud

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php require_once("header.php"); ?>
 <?php require_once("functions.php");
 
@@ -7,12 +6,90 @@
 	if(isset($_SESSION["logged_in_user_id"])){
 		header("Location: data.php");
 	}
-    ?>
-	<!-- ###################### -->
-	<!-- ####### MENÜÜ ######## -->
-	<!-- ###################### -->
+    
 	
-
+	
+	/////////////////////////MUUTUJAD ERRORITE JAOKS
+	$email1_error = "";
+	$email2_error = "";
+	$password1_error = "";
+	$password2_error = "";
+	$password3_error ="";
+	$firstname_error ="";
+	$lastname_error ="";
+	
+	/////////////////////////MUUTUJAD LOGIN
+	
+	$email1 ="";
+	$email2 ="";
+	$firstname ="";
+	$lastname ="";
+	$password1 ="";
+	$password2 ="";
+	
+	////väljade kontrollimine ja regamine.
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		if(isset($_POST['login'])){
+			if ( empty($_POST["email1"]) ) {
+				$email1_error = "See väli on kohustuslik";
+			}
+			else{
+				$email1 = test_input($_POST["email1"]);
+			}
+			if ( empty($_POST["password1"]) ) {
+				$password1_error = "See väli on kohustuslik";	
+			}
+			else{
+				$password1 = test_input($_POST["password1"]);
+			}
+			if($password1_error == "" && $email1_error == ""){
+				echo "Võib sisse logida! Kasutajanimi on ".$email1." ja parool on ".$password1;
+				$hash = hash("sha512", $password1);
+				loginUser($email1, $hash);
+			}
+		}elseif(isset($_POST['registreeri'])) {
+			if ( empty($_POST["firstname"]) ) {
+				$firstname_error = "See väli on kohustuslik";
+			}
+			else{
+				$firstname = test_input($_POST["firstname"]);
+			}			
+			if ( empty($_POST["lastname"]) ) {
+				$lastname_error = "See väli on kohustuslik";
+			}
+			else{
+				$lastname = test_input($_POST["lastname"]);
+			}
+			if ( empty($_POST["email2"]) ) {
+				$email2_error = "See väli on kohustuslik";
+			}
+			else{
+				$email2 = test_input($_POST["email2"]);
+			}
+			
+			if ( empty($_POST["password2"]) ) {
+				$password2_error = "See väli on kohustuslik";	
+			}else{
+				
+				if(strlen($_POST["password2"]) < 8) {
+					$password2_error ="Peab olema vähemalt 8 sümbolit pikk!";
+				}
+				else{
+					$password2 = test_input($_POST["password2"]);
+				}
+			}
+			
+				
+			if(	$email2_error == "" && $password2_error == ""){
+				$hash = hash("sha512", $password2);
+				echo "Võib kasutajat luua! Kasutajanimi on ".$email2." ja parool on ".$password2. "ja räsi on ".$hash;
+				createUser($firstname, $lastname, $email2, $hash);
+			}	
+		}
+	} 
+	
+?>
 	
 <br><br><br><br><br><br>
 
@@ -25,20 +102,21 @@
 	<div class="row">
 		
 		<div class="col-md-6 col-sm-5 col-sm-offset-1">
-			<h1> Tere </h1>
+			<h1> Tere tulemast Jalgpalli Foorumisse! Loo endale kasutaja ning liitu tuhandete jalgpalli
+armastajatega.</h1>
 		</div>
 		
 		<div class="col-md-3 col-sm-4 col-sm-offset-1">
 			
 			<form>
 			  <div class="form-group">
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+				<input type="email" name="email1" value="<?php echo $email1 ?>" class="form-control" id="exampleInputEmail1" placeholder="E-post"> <?php echo $email1_error; ?>
 			  </div>
 			  
 				<div class="row">
 					<div class="col-md-8">
 						<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						<input type="password" name="password1" class="form-control" id="exampleInputPassword1" placeholder="Password"> <?php echo $password1_error; ?>
 						</div>
 					</div>
 					<div class="col-md-4">
@@ -57,15 +135,15 @@
 			<form>
 				<h1>CREATE NEW USER</h1>
 			  <div class="form-group">
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+				<input type="email" class="form-control" name="email2" id="exampleInputEmail1" placeholder="Email"> <?php echo $email2_error; ?>
 			  </div>
 			  
 				<div class="row">
 					<div class="col-md-8">
 						<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-						<input type="text" class="form-control" id="exampleInputFirstName" placeholder="First name">
-						<input type="text" class="form-control" id="exampleInputLastName" placeholder="Last name">
+						<input type="password" name="password2" class="form-control" id="exampleInputPassword1" placeholder="Password"><?php echo $password2_error; ?>
+						<input type="text" name="firstname" class="form-control" id="exampleInputFirstName" placeholder="First name"><?php echo $firstname_error;?>
+						<input type="text" name="lastname" class="form-control" id="exampleInputLastName" placeholder="Last name"><?php echo $lastname_error;?>
 						</div>
 					</div>
 					<div class="col-md-4">
@@ -83,78 +161,5 @@
 	</div>
 
 </div>
-=======
-<?php require_once("header.php"); ?>
-    
 
-	
-<br><br><br><br><br><br>
-<!-- ###################### -->
-<!-- ####### SISU ######## -->
-<!-- ###################### -->	
-
-<div class="container">
-
-	<div class="row">
-		
-		<div class="col-md-6 col-sm-5 col-sm-offset-1">
-			<h1> Tere </h1>
-		</div>
-		
-		<div class="col-md-3 col-sm-4 col-sm-offset-1">
-			
-			<form>
-			  <div class="form-group">
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-			  </div>
-			  
-				<div class="row">
-					<div class="col-md-8">
-						<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-						</div>
-					</div>
-					<div class="col-md-4">
-					<button type="submit" class="btn btn-info hidden-xs ">Login</button>
-					<button type="submit" class="btn btn-info btn-block visible-xs">Login</button>
-					</div>
-					
-				
-			  </div>
-			  
-			</form>
-			
-			<br><br>
-			
-			
-			<form>
-				<h1>CREATE NEW USER</h1>
-			  <div class="form-group">
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-			  </div>
-			  
-				<div class="row">
-					<div class="col-md-8">
-						<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-						<input type="text" class="form-control" id="exampleInputFirstName" placeholder="First name">
-						<input type="text" class="form-control" id="exampleInputLastName" placeholder="Last name">
-						</div>
-					</div>
-					<div class="col-md-4">
-					<button type="submit" class="btn btn-info hidden-xs ">Login</button>
-					<button type="submit" class="btn btn-info btn-block visible-xs">Login</button>
-					</div>
-					
-				
-			  </div>
-			  
-			</form>
-			
-		</div>
-		
-	</div>
-
-</div>
->>>>>>> ae01d6a486725dcf10f3089662b1e3125401cf2a
 	
