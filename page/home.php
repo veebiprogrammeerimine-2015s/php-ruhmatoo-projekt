496970a151dbdbd5dbf611e89d029d7abe3c7da8
Katariina Linde <klinde@greeny.cs.tlu.ee>
2016-01-05 23:17:30 +0200

    home page

--- page/home.php
+++ page/home.php
@@ -80,13 +80,4 @@
     //faili nimi
     $page_file_name="home.php";
 ?>
-<?php
-    require_once("../header.php");
-?>
 
-<h2>Jooks24 on mõeldud kõikidele jooksusõpradele. See on abistav ja informeeriv sait, mis hõlmab kõikvõimalikke Eestis toimuvaid jooksuüritusi ning nendele registreerumist ja hiljem ka kogemuste kirjeldust!</h2>
-
-<?php
-    require_once("../footer.php");
-?> 
->>>>>>> e517b961873c1cca8fe1fa834a06fea62c0d4896
