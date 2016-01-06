4a5aff8541d4f373d5298a5b47a6cb27cb68421e
Koidu Kannela <Koidu@users.noreply.github.com>
2016-01-06 18:03:42 +0200

    user.class.php

--- pages/user.class.php
+++ pages/user.class.php
@@ -10,7 +10,7 @@ class User {
 		
 		$response = new StdClass();
 		
-		$stmt = $this->connection->prepare("SELECT email FROM disc_users WHERE email =?");
+		$stmt = $this->connection->prepare("SELECT email FROM users_php WHERE email =?");
 		$stmt->bind_param("s",$create_email);
 		$stmt->execute();		
 		
@@ -28,7 +28,7 @@ class User {
 		
 		$stmt->close();
 		
-		$stmt = $this->connection->prepare("INSERT INTO disc_users (name, email, password) VALUES (?, ?, ?)");
+		$stmt = $this->connection->prepare("INSERT INTO users_php (name, email, password) VALUES (?, ?, ?)");
 		$stmt->bind_param("sss",$create_name, $create_email, $password_hash);
 		
 		if($stmt->execute()){
@@ -52,7 +52,7 @@ class User {
 		
 		$response = new StdClass();
 		
-		$stmt = $this->connection->prepare("SELECT email FROM disc_users WHERE email=?");
+		$stmt = $this->connection->prepare("SELECT email FROM users_php WHERE email=?");
 		$stmt->bind_param("s", $email);
 		$stmt->execute();
 		
@@ -67,7 +67,7 @@ class User {
 		
 		$stmt->close();
 		
-		$stmt = $this->connection->prepare("SELECT id, email FROM disc_users WHERE email=? AND password=?");
+		$stmt = $this->connection->prepare("SELECT user_id, email FROM users_php WHERE email=? AND password=?");
 		$stmt->bind_param("ss", $email, $password_hash);
 		$stmt->bind_result($id_from_db, $email_from_db);
 		$stmt->execute();
