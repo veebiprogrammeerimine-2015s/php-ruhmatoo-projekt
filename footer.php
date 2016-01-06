079c9599fd02b90191355ff6cae3e7f8fbfba905
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2016-01-06 15:06:59 +0200

    #5 loodud form

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
+<script>
+
+$(document).ready(function() {
+
+ var docHeight = $(window).height();
+ var footerHeight = $('.page-footer').height();
+ var footerTop = $('.page-footer').position().top + footerHeight;
+
+ if (footerTop < docHeight) {
+  $('.page-footer').css('margin-top', 1+ (docHeight - footerTop) + 'px');
+ }
+});
+</script>
 
 <div class="page-footer">
-	
+
   <div class="row">
 	  <div class="col-xs-6 col-md-3 col-md-offset-1">Something interesting</div>
 	  <div class="col-xs-6 col-md-3">Telefoni nr</div>
@@ -9,16 +22,16 @@
   <ul class="nav nav-pills">
   <li role="presentation" class="active"><a href="#">Back to top</a></li>
   </ul>
-  
+
   </div>
-  
-  
+
+
   <div class="row">
 	  <div class="col-xs-6 col-md-3 col-md-offset-1">RateMyPro OY</div>
 	  <div class="col-xs-6 col-md-3">Olematu mnt 112b</div>
 	  <div class="col-xs-6 col-md-3">info@ratemypro.ee</div>
 
-  
+
   </div>
 </div>
 
---
+++ page/addprof.php
@@ -0,0 +1,72 @@
+<?php
+  require_once("../header.php");
+	require_once("../functions.php");
+  //Lehe nimi
+  $page_title = "Uus professor";
+  //Faili nimi
+  $page_file = "addprof.php";
+
+?>
+<?php
+  $firstname_error = $lastname_error = "";
+  $firstname = $lastname = "";
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+      if(isset($_POST["add"])){
+      if (empty($_POST["firstname"]) ) {
+        $firstname_error = "See väli on kohustuslik";
+      }else{
+        $firstname = cleanInput($_POST["firstname"]);
+      }
+
+      if (empty($_POST["lastname"]) ) {
+        $lastname_error = "See väli on kohustuslik";
+      }else{
+        $lastname = cleanInput($_POST["lastname"]);
+      }
+
+      if (empty($_POST["school"]) ) {
+        $school_error = "See väli on kohustuslik";
+      }else{
+        $school = cleanInput($_POST["lastname"]);
+      }
+
+      if ($firstname_error == "" && $lastname_error == "" && $school_error == "") {
+        $response = $Rate->newProfessor($firstname, $lastname, $school);
+      }
+    }
+  }
+
+ ?>
+<div class="row">
+  <div class="col-sm-12">
+    <div class="col-sm-6">
+      <h1>Uus professor</h1>
+      <p>Kas leidsid professori, kes on puudu?</p>
+      </div>
+      <div class="col-sm-6">
+        <h1>Uus professor</h1>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+          <label for="firstname">Eesnimi</label>
+          <input type="text" class="form-control" name="firstname">
+          <?=$firstname_error;?>
+          <br>
+          <label for="lastname">Perekonnanimi</label>
+          <input type="text" class="form-control" name="lastname">
+          <?=$lastname_error;?>
+          <br>
+          SELECT
+          <?=$school_error;?>
+          <br>
+          <input type="submit" name="add" class="btn btn-primary pull-right">
+        </form>
+      </div>
+    </div>
+
+
+
+</div>
+
+
+
+<?php require_once("../footer.php"); ?>
