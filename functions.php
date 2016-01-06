7cd48c5cde1948fc276ef0a4a8a571f95a4fff0e
Koidu Kannela <Koidu@users.noreply.github.com>
2016-01-06 17:27:43 +0200

    user.class.php

--- functions.php
+++ functions.php
@@ -1,47 +1,17 @@
 <?php
 	
 	require_once("../../teamconfig.php");
+	require_once("user.class.php");
 	
+	$database = "if15_jarmhab";
 	
 	session_start();
 	
-	function createUser($create_name, $create_email, $password_hash){
-		
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		
-		$stmt = $mysqli->prepare("INSERT INTO disc_users (name, email, password) VALUES (?, ?, ?)");
-		$stmt->bind_param("sss",$create_name, $create_email, $password_hash);
-		$stmt->execute();
-		$stmt->close();
-		
-		$mysqli->close();		
-	}
+	$mysqli = new mysqli($servername, $server_username, $server_password, $database);
 	
-	function loginUser($email, $password_hash){
-		
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		
-		$stmt = $mysqli->prepare("SELECT id, email FROM disc_users WHERE email=? AND password=?");
-		$stmt->bind_param("ss", $email, $password_hash);
-		$stmt->bind_result($id_from_db, $email_from_db);
-		$stmt->execute();
-		if($stmt->fetch()){
-			echo "kasutaja id=".$id_from_db;
-			
-			$_SESSION["id_from_db"] = $id_from_db;
-			$_SESSION["user_email"] = $email_from_db;
-			
-			
-		header("Location: main.php");
-			
-			
-		}else{
-			echo "Wrong password or email!";
-		}
-		$stmt->close();
-		
-		$mysqli->close();
-	}
+	$User = new User($mysqli);
+	
+
 
 	function getParkData(){
 		
