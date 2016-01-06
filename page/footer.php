27cb096485822b416df5d0381b6e5d6ecab5a1ec
Stenver Kööp <stenkoo@greeny.cs.tlu.ee>
2016-01-06 10:20:18 +0200

    initial files

---
+++ page/footer.php
@@ -0,0 +1,4 @@
+	
+	<p><i>Made by PollNet Tiim, 2015<i><p>
+	</body>
+</html>
---
+++ page/header.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title><?php echo $page_title ?></title>
+	</head>
+	<body>
+	<?php
+		//laeme menu.php faili sisu
+		require_once ("menu.php");
+	?>
