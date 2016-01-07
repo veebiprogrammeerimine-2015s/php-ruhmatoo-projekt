036aba3dd29ddbf6a050335b20d061ee1c0ed6f9
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2016-01-07 14:29:29 +0200

    login ja register frontend

--- register.php
+++ register.php
@@ -8,23 +8,21 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6 col-md-4 col-md-offset-4">
-            <h1 class="text-center login-title">Logi sisse</h1>
+            <h1 class="text-center login-title">Registreeri</h1>
             <div class="account-wall">
                 <img class="profile-img" src="pildid/jalgpall.ico"
                     alt="sveg">
                 <form class="form-signin">
-                <input type="text" class="form-control" name="email1" placeholder="Email" required autofocus>
+                <input type="text" class="form-control" placeholder="Email" name="email2" required autofocus>
+                <input type="text" class="form-control" placeholder="Nimi" name="name" required autofocus>
                 <input type="password" class="form-control" name="parool2" placeholder="Parool" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">
-                    Logi sisse</button>
-                <label class="checkbox pull-left">
-                    <input type="checkbox" value="remember-me">
-                    Jäta mind meelde
-                </label>
+                    Registreeri</button>
+                
                 
                 </form>
             </div>
-            <a href="register.php" class="text-center new-account">Loo kasutaja </a>
+            
         </div>
     </div>
 </div>
