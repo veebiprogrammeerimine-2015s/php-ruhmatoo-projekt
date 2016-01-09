9189a5bb4093f0e6d45177b58d30ecfa6f1260e2
Merit Paist <meripai@greeny.cs.tlu.ee>
2016-01-09 19:58:50 +0200

    header

--- page/header.php
+++ page/header.php
@@ -1,1 +1,13 @@
 Kasutaja: <?=$_SESSION['logged_in_user_email'];?> <a href="?logout=1" style="text-decoration:none">[logi välja]</a>
+
+<?php
+if(!isSet($_SESSION["logged_in_user_id"])){
+		header("Location: login.php");
+		exit();
+	}
+	
+	if(isSet($_GET["logout"])){
+		session_destroy();
+		header("Location: login.php");
+	}
+?>
