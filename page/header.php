4dc1acb38d17d922b5d892e5306206704792da1d
Merit Paist <meripai@greeny.cs.tlu.ee>
2016-01-09 21:51:06 +0200

    header

--- page/header.php
+++ page/header.php
@@ -1,4 +1,52 @@
-Kasutaja: <?=$_SESSION['logged_in_user_email'];?> <a href="?logout=1" style="text-decoration:none">[logi v�lja]</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+	<title><?php echo $page_title ?></title>
+	<script type="text/javascript" src="functions.js"></script>
+</head>
+<body>
+	
+	<!-- ÜLEMINE MENÜÜ --->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#">No plagiarism</a>
+	</div>
+
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="history.php">Ajalugu<span class="sr-only">(current)</span></a></li>
+			<li><a href="data.php">Tellimuse esitamine</a></li>
+			<li><a href="requests.php">Tööpakkumised</a></li>
+			<li><a href="offers.php">Ajakirjanike pakkumised</a></li>
+			<li><a href="feedback.php">Tagasiside</a></li>
+			<?php 
+				if(isset($page_file_name) && $page_file_name != "data.php") { 
+					echo '<li><a href="data.php">Tellimuse esitamine</a></li>';
+				} else { 
+					echo '<li>Tellimuse esitamine</li>';
+				} 
+			?>
+	
+		</ul>
+	</div>
+	</div>
+	</nav><br><br><br>
+	
+Kasutaja: <?=$_SESSION['logged_in_user_email'];?> <a href="?logout=1" style="text-decoration:none">[logi välja]</a>
+
 
 <?php
 if(!isSet($_SESSION["logged_in_user_id"])){
@@ -11,3 +59,4 @@ if(!isSet($_SESSION["logged_in_user_id"])){
 		header("Location: login.php");
 	}
 ?>
+
