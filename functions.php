2ebb72d6f12bf784d05a3bf650ef6f8b2aebbf97
earist <earistt@gmail.com>
2016-01-12 00:58:57 +0200

    Delete functions.php

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once("../config_global.php");
-	
-	$database = "if15_teamalpha_3";
-	
-	session_start();
-	
-	$mysqli = new mysqli($servername, $server_username, $server_password, $database);
-?>
