706891ccfb02d523a9ff5d3f8c2ca40e6ab050f5
Merilin Takk <merilintakk@hotmail.com>
2016-01-11 21:26:28 +0200

    Lisasin logimiseks vormi htmli

--- app/View/login.php
+++ app/View/login.php
@@ -1,3 +1,32 @@
-<?php include(__DIR__.'/_partials/header.php'); ?>
-    login
-<?php include(__DIR__.'/_partials/footer.php'); ?>
+<?php include(__DIR__ . '/_partials/header.php'); ?>
+    <form class="form-horizontal" method="post">
+        <fieldset>
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="username">Kasutaja</label>
+
+                <div class="col-md-5">
+                    <input id="username" name="username" placeholder="Mari445" class="form-control input-md" required=""
+                           type="text">
+                </div>
+            </div>
+            <!-- Password input-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="password">Parool</label>
+
+                <div class="col-md-5">
+                    <input id="password" name="password" placeholder="***********" class="form-control input-md"
+                           type="password">
+                </div>
+            </div>
+            <!-- Button -->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="singlebutton"></label>
+
+                <div class="col-md-4">
+                    <button id="singlebutton" name="singlebutton" class="btn btn-info">Logi sisse!</button>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+<?php include(__DIR__ . '/_partials/footer.php'); ?>
