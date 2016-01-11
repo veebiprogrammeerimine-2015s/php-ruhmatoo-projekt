5091f29d21c9859c61ab13152bd0b40c3169e83c
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2016-01-11 20:17:52 +0200

    changes

--- page/index.php
+++ page/index.php
@@ -1,12 +1,12 @@
 <?php require_once("../header.php"); ?>
 
-<class="span12">
-	<div style="padding:0 35px">
-		<div class="row-fluid">
-			<h3>Ajakirjaniku võimalus leida endale vaheldusrikas töö!</h3>
-			<h3>Ettevõtte võimalus leida oma firmat esindavale tekstile hea tegija - küsi pakkumisi, võrdle ja vali parim!</h3>
+	<class="span12">
+		<div style="padding:0 35px">
+			<div class="row-fluid">
+				<h3>Ajakirjaniku võimalus leida endale vaheldusrikas töö!</h3>
+				<h3>Ettevõtte võimalus leida oma firmat esindavale tekstile hea tegija - küsi pakkumisi, võrdle ja vali parim!</h3>
+			</div>
 		</div>
 	</div>
-</div>
 
 <?php require_once("../footer.php"); ?>
