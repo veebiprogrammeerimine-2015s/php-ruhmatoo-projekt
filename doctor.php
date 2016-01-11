8f988ec3f73ca88faa79f6f95b344c74d1488d53
Helen <helenpuhu@gmail.com>
2016-01-11 17:52:00 +0200

    edited html/css

--- doctor.php
+++ doctor.php
@@ -83,26 +83,95 @@
 	
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LOOMAKLIINIK - Log in</title>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="materialize.min.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+</html>
+
 <p>
 	Tere, <?=$_SESSION["logged_in_user_email"];?> 
-	<a href="?logout=1"> Logi välja <a> 
+	<a href="?logout=1"> Logi välja </a> 
 </p>
 
-<h2>Arsti vaade</h2>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
-	<label for="doctor_name" >Arsti nimi</label><br>
-	<input id="doctor_name" name="doctor_name" type="text" value="<?php echo $doctor_name; ?>"> <?php echo $doctor_name_error; ?><br><br>
-	<label for="animal_name" >Protseduuri nimetus</label><br>
-	<input id="procedure_name" name="procedure_name" type="text" value="<?php echo $procedure_name; ?>"> <?php echo $procedure_name_error; ?><br><br>
-	<label for="operation_name" >Operatsiooni nimetus</label><br>
-	<input id="operation_name" name="operation_name" type="text" value="<?php echo $operation_name; ?>"> <?php echo $operation_name_error; ?><br><br>
-	<label for="operation_date" >Operatsiooni kuupäev</label><br>
-	<input id="operation_date" name="operation_date" type="date" value="<?php echo $operation_date; ?>"> <?php echo $operation_date_error; ?><br><br>
-	<label for="operation_difficulty" >Operatsiooni raskustase</label><br>
-	<input id="operation_difficulty" name="operation_difficulty" type="text" value="<?php echo $operation_difficulty; ?>"> <?php echo $operation_difficulty_error; ?><br><br>
-	<label for="d_animal_name" >Looma nimi</label><br>
-	<form id="d_animal_name" name="d_animal_name" type="text" value="<?php echo $d_animal_name; ?>"> <?php echo $d_animal_name_error; ?>
-	<?=$dropdown->createDropdown();?>
-	</form><br><br>
-	<input type="submit" name="docto" value="Salvesta">
+<div class="doctor-view">
+<h2 class="doctor-h2">Arsti vaade</h2>
+<form class="col s12" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<div class="row">
+	<div class="input-field col s4">
+		<label for="doctor_name">Arsti nimi</label>
+		<input id="doctor_name" name="doctor_name" type="text" value="<?php echo $doctor_name; ?>"> <?php echo $doctor_name_error; ?>
+	</div>
+	</div>
+	<div class="row">
+	<div class="input-field col s4">
+		<label for="animal_name">Protseduuri nimetus</label>
+		<input id="procedure_name" name="procedure_name" type="text" value="<?php echo $procedure_name; ?>"> <?php echo $procedure_name_error; ?>
+	</div>
+	</div>
+	<div class="row">
+	<div class="input-field col s4">
+		<label for="operation_name" >Operatsiooni nimetus</label>
+		<input id="operation_name" name="operation_name" type="text" value="<?php echo $operation_name; ?>"> <?php echo $operation_name_error; ?>
+	</div>
+	</div>
+	<div class="row">
+	<div class="input-field col s4">
+		<label for="date">Operatsiooni kuupäev</label>
+		<input type="date" class="datepicker" value="<?php echo $operation_date; ?>"> <?php echo $operation_date_error; ?>
+	</div>
+	</div>
+	<form action="#">
+	
+	<label for="operation_difficulty">Operatsiooni raskustase</label>
+	<p>
+	<input name="tase1" type="radio" id="tase1" value="<?php echo $operation_difficulty; ?>"> <?php echo $operation_difficulty_error; ?>
+	<label for="tase1">1</label>
+	</p>
+	<p>
+	<input name="tase2" type="radio" id="tase2" value="<?php echo $operation_difficulty; ?>"> <?php echo $operation_difficulty_error; ?>
+	<label for="tase2">2</label>
+	</p>
+	<p>
+	<input name="tase3" type="radio" id="tase3" value="<?php echo $operation_difficulty; ?>"> <?php echo $operation_difficulty_error; ?>
+	<label for="tase3">3</label>
+	</p>
+	<p>
+	<input name="tase4" type="radio" id="tase4" value="<?php echo $operation_difficulty; ?>"> <?php echo $operation_difficulty_error; ?>
+	<label for="tase4">4</label>
+	</p>
+	<p>
+	<input name="tase5" type="radio" id="tase5" value="<?php echo $operation_difficulty; ?>"> <?php echo $operation_difficulty_error; ?>
+	<label for="tase5">5</label>
+	</p>
+	</form>
+	<label for="d_animal_name" >Looma nimi</label>
+	<form>
+	<<?=$dropdown->createDropdown();?>id="d_animal_name" name="d_animal_name" type="text" value="<?php echo $d_animal_name; ?>"> <?php echo $d_animal_name_error; ?>
+	</form>
+	<a class="waves-effect waves-light btn" type="submit" name="register">Salvesta</a>
+</div>
 </form>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="materialize.min.js"></script>
+
+ <script>
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+  </script>
+
+</body>
+</html>
+<?php
+require("footer.html");
+?>
