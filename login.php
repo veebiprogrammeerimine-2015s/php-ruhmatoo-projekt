ef547f9857129daa46594c75cdb2195e2b89ab62
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2016-01-12 11:00:42 +0200

    lisad

--- login.php
+++ login.php
@@ -1,117 +1,64 @@
-<?php require_once("header.php"); ?>
-
-
-	<!-- ###################### -->
-	<!-- ####### MENUU ######## -->
-	<!-- ###################### -->
-	
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="http://localhost:5555/~rusljeg/php-ruhmatoo-projekt/tiitelleht.html">Eesti post</a>
-    </div>
-</nav>
-	
-<br><br><br>
-
-
-	
-
-
-
-<?php
-	require_once("functions.php");
-	
-	if(isset($_SESSION["logged_in_user_id"])){
-		header("Location: data.php");
-	}
-	
-	$email = "";
-	$login_email = "";
-	$email_error = "";
-	
-	$password = "";
-	$login_password = "";
-	$password_error = "";
-	
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-		if(isset($_POST["login"])){
-
-			if ( empty($_POST["email"]) ) {
-				$email_error = "See väli on kohustuslik";
-			}else{
-
-				$email = cleanInput($_POST["email"]);
-			}
-			if ( empty($_POST["password"]) ) {
-				$password_error = "See väli on kohustuslik";
-			}else{
-				$password = cleanInput($_POST["password"]);
-			}
-
-			if($password_error == "" && $email_error == ""){
-
-				echo "Võib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
-				
-
-				
-				if(isset($login_response->success)){
-					
-
-					$_SESSION["logged_in_user_id"] = $login_response->user->id;
-					$_SESSION["logged_in_user_email"] = $login_response->user->email;
-
-					
-
-					$_SESSION["login_success_message"] = $login_response->success->message;
-					
-					header("Location: data.php");
-					
-					
-				}
-				
-				
-			}
-		}
-	}
-
-  function cleanInput($data) {
-  	$data = trim($data);
-  	$data = stripslashes($data);
-  	$data = htmlspecialchars($data);
-  	return $data;
-  }
-	
-	
-	
-?>
 <!DOCTYPE html>
-<html>
-<head>
-  <title>Login</title>
-</head>
-<body>
 
-  <h2>Log in</h2>
-  
-  <?php if(isset($login_response->error)): ?>
-	<p style="color:red;"> <?=$login_response->error->message;?> </p>
-  <?php elseif(isset($login_response->success)): ?>
-	<p style="color:green;"> <?=$login_response->success->message;?> </p>
-  <?php endif; ?>
-  
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
-  	<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?><br><br>
-  	<input name="password" type="password" placeholder="Parool" value="<?php echo $password; ?>"> <?php echo $password_error; ?><br><br>
+<html lang="en">
+  <head>
+	<link rel="stylesheet" type="text/css" href="header.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eesti post</title>
+
+
+    <!-- Bootstrap -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" >
+<link rel="stylesheet" href="style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<base target="_self">
+	</head>
+  <body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="tiitelleht.html">Eesti post</a>
+		  <a class='navbar-brand-logout' href='?logout=1'>Logi välja</a>		</div>
+	</nav>
+	<br><br><br>
+
+
+
+  <div id="login" align="center">
+  <h2">Logi sisse</h2>
+    
+  <form action="/~janekos/tunnitööd/veebiprogrameerimine/php-ruhmatoo-projekt/login.php" method="post" >
+  	<input name="email" type="email" placeholder="E-post" value=""> <br><br>
+  	<input name="password" type="password" placeholder="Parool" value=""> <br><br>
   	<input type="submit" name="login" value="Log in">
   </form>
-  
-<?php require_once("footer.php"); ?>
+  </div>
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" ></script>
+		<footer>
+			<p>Kontakt     E-post: name@gmail.com     Tel. number: +372 5348 7792</p>
+		</footer>
+	</body>
+</html>
