cc968d79fbd697bb9331b0e689f6b1999b24b5f4
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2016-01-13 18:16:49 +0200

    foorumi katsetus

--- functions.php
+++ functions.php
@@ -56,9 +56,24 @@
 		// Global muutujad, et kätte saada config failist andmed
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
-		$stmt = $mysqli->prepare("INSERT INTO eestijalgpall (post) VALUES (?)");
-		$stmt->bind_param("s", $arvamus);
-		$stmt->execute();
+		$stmt = $mysqli->prepare("INSERT INTO eestijalgpall (user_id, user_email, post) VALUES (?,?,?)");
+		$stmt->bind_param("iis", $_SESSION["logged_in_user_id"], $_SESSION["logged_in_user_email"], $arvamus);
+		if($stmt->execute()){
+			// kui on tõene,
+			//siis INSERT õnnestus
+			$message = "Sai edukalt lisatud";
+			 
+			
+		}else{
+			// kui on väärtus FALSE
+			// siis kuvame errori
+			echo $stmt->error;
+			
+		}
+		
+		return $message;
+		
+		
 		$stmt->close();
 		
 		$mysqli->close();
