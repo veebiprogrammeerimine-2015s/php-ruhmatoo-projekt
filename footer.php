61353aebb221185040fb5ffd6dd5dd6593ad9222
devweb.eu <vhost45490ssh@regulus.elkdata.ee>
2016-01-27 15:29:22 +0000

    #17 loodud footer + js footeri asukoha m22ramiseks

--- footer.php
+++ footer.php
@@ -1,13 +1,37 @@
 
 </div>
-<!--<footer class="navbar navbar-default-bottom">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<p><i>Made by Rauno</i></p>
-			</div>
+<footer id="footer" class="footer">
+  <div class="container">
+    <p class="text-muted" style="margin-top: 5px;">Toetajad:</p>
+		<div class="col-xs-6 col-sm-3" style="background-color: #ccc; height: 70px; border: 1px dashed #777777"></div>
+		<div class="col-xs-6 col-sm-3" style="background-color: #ccc; height: 70px; border: 1px dashed #777777"></div>
+		<div class="col-xs-6 col-sm-3" style="background-color: #ccc; height: 70px; border: 1px dashed #777777"></div>
+		<div class="col-xs-6 col-sm-3" style="background-color: #ccc; height: 70px; border: 1px dashed #777777"></div>
+
+		<div class="col-sm-4" style="padding-left: 0px;">
+		<p class="text-muted" style="margin-top: 5px;">Website by: <a href="http://devweb.eu" style="color: #777777">DevWeb</a></p>
 		</div>
-	</div>
-</footer>-->
+
+		<div class="col-sm-4 left-center">
+		<p class="text-muted left-center" style="margin-top: 5px;">Noorte Tööbörs 2015</a></p>
+		</div>
+
+		<div class="col-sm-4 left-right" style="padding-right: 0px;">
+		<p class="text-muted" style="margin-top: 5px;">All rights reserved</a></p>
+		</div>
+
+  </div>
+</footer>
+
+<script>
+
+	$('#footer').css('margin-top',
+	  $(document).height()
+	  - ( $('#header').height() + $('#movingit').height() )
+	  - $('#footer').height()
+	);
+
+</script>
+
 </body>
 </html>
