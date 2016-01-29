898e4b582e626d909f3364ff971ab671d6487d8e
devweb.eu <vhost45490ssh@regulus.elkdata.ee>
2016-01-29 13:58:23 +0000

    #17 CV saatmine disainitud

--- job/sendresume.php
+++ job/sendresume.php
@@ -20,11 +20,6 @@
 	$trimmed = rtrim($file_to_trim, ".php");
 	$currentJob = $Job->singleJobData($trimmed);
 
-	echo "<h3>".$currentJob->name."</h3>";
-  echo "Ettevõte:".$currentJob->company;
-  echo "<br>Töö kirjeldus:".$currentJob->description;
-  echo "<h3>Asukoht:</h3><p>".$currentJob->county."<br>".$currentJob->parish."<br>".$currentJob->location."<br>".$currentJob->address."</p>";
-
   if(isset($_SESSION['logged_in_user_id'])) {
     if($_SESSION['logged_in_user_group'] == 1) {
       if( $_SERVER["REQUEST_METHOD"] == "POST") {
@@ -38,22 +33,44 @@
     }
   }
 ?>
-<div class="col-sm-12">
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-<label for="motivation">Motivatsioonikiri</label>
-<textarea class="form-control" rows="6" name="motivation" type="text"></textarea>
+<div class="col-sm-6">
+<h3>Töökuulutus</h3>
+<div class="list-group">
+  <div id="job_offer" class="list-group-item">
+    <h4 class="list-group-item-heading"><strong><?=$currentJob->name;?></strong></h4>
+    <p class="list-group-item-text"><?=$currentJob->company;?></p>
+		<p style="height: 1px; margin: 9px 0; overflow: hidden; background-color: #e5e5e5;"></p>
+		<p class="list-group-item-text"><?=$currentJob->description;?></p>
+		<p style="height: 1px; margin: 9px 0; overflow: hidden; background-color: #e5e5e5;"></p>
+		<p class="list-group-item-text"><?=$currentJob->county."<br>".$currentJob->parish."<br>".$currentJob->location."<br>".$currentJob->address;?></p>
+  </div>
 </div>
-<br>
+
+</div>
+
+<div class="col-sm-6">
+<h3>CV saatmine</h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+	<div class="list-group">
+	  <div id="application" class="list-group-item">
+			<label>Vali CV</label>
+	    <select name="my_resume" class="form-control">
+				<?php for($i = 0; $i < count($my_resumes); $i++){
+					echo '<option value="'.$my_resumes[$i]->id.'">'.$my_resumes[$i]->name.'</option>';
+
+				}?>
+			</select>
+			<p style="height: 1px; margin: 9px 0; overflow: hidden; background-color: #e5e5e5;"></p>
+			<label>Motivatsioonikiri</label>
+			<textarea class="form-control" rows="6" name="motivation" type="text"></textarea>
+	  </div>
+	</div>
+
+</div>
 
 <div class="col-sm-6">
 	<br>
-	<select name="my_resume" class="form-control">
-		<?php for($i = 0; $i < count($my_resumes); $i++){
-			echo '<option value="'.$my_resumes[$i]->id.'">'.$my_resumes[$i]->name.'</option>';
 
-		}?>
-	</select>
 </div>
 
 <div class="col-sm-6">
@@ -66,7 +83,21 @@
 </form>
 
 
+<script>
 
+	var job_height = document.getElementById("job_offer").offsetHeight;
+	var application_height = document.getElementById("application").offsetHeight;
+
+	if (job_height > application_height) {
+		document.getElementById("application").style.height = job_height + "px";
+
+	} else {
+		document.getElementById("job_offer").style.height = application_height + "px";
+
+	}
+
+
+</script>
 
 
 
